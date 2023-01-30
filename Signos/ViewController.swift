fe445bfa48700ab4f21a58a8da281f532011f7d0
Felipe Augusto <augusto_felipe@icloud.com>
2023-01-30 16:20:27 -0300

    Initial Commit

---
+++ Signos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Signos
+//
+//  Created by Felipe Augusto Correia on 30/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
